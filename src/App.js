e5b3d928aceac65bb3188966df18e328ee59e510
Saurabh Potdar <royalchess98@gmail.com>
2021-02-01 13:11:19 +0530

    Slider commit 2

--- src/App.js
+++ src/App.js
@@ -9,104 +9,84 @@ import './App.css';
 const App = () =>{
   const [ques, setQues] = useState([
     {
-      "userId": 1,
       "id": 1,
       "title": "Rate the Hotel"
     },
     {
-      "userId": 1,
       "id": 2,
       "title": "Rate the Rooms"
     },
     {
-      "userId": 1,
       "id": 3,
       "title": "Rate the Canteen"
     },
     {
-      "userId": 1,
       "id": 4,
       "title": "Rate the Food"
     },
     {
-      "userId": 1,
       "id": 5,
       "title": "Rate the Travelling"
     },
     {
-      "userId": 1,
       "id": 6,
       "title": "Rate the Table"
     },
     {
-      "userId": 1,
       "id": 7,
       "title": "Rate the Car"
     },
     {
-      "userId": 1,
       "id": 8,
       "title": "Rate the Bike"
     },
     {
-      "userId": 1,
       "id": 9,
       "title": "Rate the Places"
     },
     {
-      "userId": 1,
       "id": 10,
       "title": "Rate the Staff"
     },
     {
-      "userId": 2,
       "id": 11,
-      "title": "et ea vero quia laudantium autem"
+      "title": "Rate the Hotel"
     },
     {
-      "userId": 2,
       "id": 12,
-      "title": "in quibusdam tempore odit est dolorem"
+      "title": "Rate the Rooms"
     },
     {
-      "userId": 2,
       "id": 13,
-      "title": "dolorum ut in voluptas mollitia et saepe quo animi"
+      "title": "Rate the Canteen"
     },
     {
-      "userId": 2,
       "id": 14,
-      "title": "voluptatem eligendi optio"
+      "title": "Rate the Food"
     },
     {
-      "userId": 2,
       "id": 15,
-      "title": "eveniet quod temporibus"
+      "title": "Rate the Travelling"
     },
     {
-      "userId": 2,
       "id": 16,
-      "title": "sint suscipit perspiciatis velit dolorum rerum ipsa laboriosam odio"
+      "title": "Rate the Table"
     },
     {
-      "userId": 2,
       "id": 17,
-      "title": "fugit voluptas sed molestias voluptatem provident"
+      "title": "Rate the Car"
     },
     {
-      "userId": 2,
       "id": 18,
-      "title": "voluptate et itaque vero tempora molestiae"
+      "title": "Rate the Bike"
     },
     {
-      "userId": 2,
       "id": 19,
-      "title": "adipisci placeat illum aut reiciendis qui"
+      "title": "Rate the Places"
     },
     {
-      "userId": 2,
       "id": 20,
-      "title": "doloribus ad provident suscipit at"
+      "title": "Rate the Staff"
     },
   ]);
 
